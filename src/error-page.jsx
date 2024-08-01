8520468382c7aa1dc57a4f30195fd41e5fe46598
Ugzher <ugo.mateda@gmail.com>
2024-08-01 16:07:03 +0200

    Error Page

---
+++ src/error-page.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import { css } from "../styled-system/css";
+import { responsiveStyle } from "./lib/sharedStyles.js";
+
+export default function ErrorPage() {
+  const layoutStyle = {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    alignItems: "space-between",
+    textAlign: "center",
+    gap: "139px",
+    "@media (max-width:640px)": {
+      gap: "113px",
+    },
+  };
+
+  const titleStyle = {
+    color: "#FF6060",
+    fontSize: "18rem",
+    height: "100%",
+    lineHeight: "100%",
+    fontWeight: "bold",
+    "@media (max-width:640px)": {
+      fontSize: "6rem",
+      pt: "50px",
+    },
+  };
+
+  const subTitleStyle = {
+    color: "#FF6060",
+    fontSize: "2.25rem",
+    fontWeight: "500",
+    "@media (max-width:640px)": {
+      fontSize: "1.125rem",
+    },
+  };
+
+  const navLink = {
+    fontWeight: "500",
+    textDecoration: "underline",
+    fontSize: "1.125rem",
+    "@media (max-width:640px)": {
+      fontSize: "0.875rem",
+    },
+  };
+  return (
+    <>
+      <Header />
+      <div className={css(layoutStyle, responsiveStyle)}>
+        <h1 className={css(titleStyle)}>404</h1>
+        <p className={css(subTitleStyle)}>
+          {"Oups! La page que vous demandez n'existe pas."}
+        </p>
+        <Link to="/" className={css(navLink)}>
+          {"Retourner sur la page d'accueil"}
+        </Link>
+      </div>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./routes/index";
 
+import ErrorPage from "./error-page.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+    errorElement: <ErrorPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
